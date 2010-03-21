38f259542f908d95c571e5376af2dd1a4d254fa7
biern <biern.m@gmail.com>
2010-03-21 20:49:47 +0100

    Added simple install script

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='Photo-sorter',
+      description='Photo sorting utility',
+      version='0.1',
+      scripts=['photo_sorter.py'],
+      author='Marcin Biernat',
+      author_email='biern.m@gmail.com',
+      url='http://github.com/biern/Photo-sorter'
+      )
